8779ade94a8822b7af9ca7faa21ac20162df5f57
Heroku-Back <143542266+Heroku-Back@users.noreply.github.com>
2024-12-12 20:30:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
-npm i -g npm
-pip3 install -r requirements.txt
+pip install -r ./requirements.txt
