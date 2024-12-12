a8e33b2495f6a65ff1143cd376d33fc04da15e0d
Heroku-Back <143542266+Heroku-Back@users.noreply.github.com>
2024-12-12 16:27:42 +0300

    Update __main__.py

--- zlzl/__main__.py
+++ zlzl/__main__.py
@@ -5,26 +5,20 @@ from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, autoname, autovars, saves
+from .utils import mybot, saves, autoname
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-LOGS = logging.getLogger("ZTele")
+LOGS = logging.getLogger("Zelzal")
 cmdhr = Config.COMMAND_HAND_LER
 
-try:
-    LOGS.info("⌭ جـارِ تحميـل الملحقـات ⌭")
-    zedub.loop.run_until_complete(autovars())
-    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
-except Exception as e:
-    LOGS.error(f"- {e}")
-
-if not Config.ALIVE_NAME:
+if gvarstatus("ALIVE_NAME") is None: #Code by T.me/zzzzl1l
     try:
         LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
         zedub.loop.run_until_complete(autoname())
         LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
     except Exception as e:
-        LOGS.error(f"- {e}")
+        LOGS.error(f"- The AutoName {e}")
 
 try:
     LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
@@ -34,10 +28,10 @@ except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
 
-class CatCheck:
+class ZTCheck:
     def __init__(self):
         self.sucess = True
-Catcheck = CatCheck()
+ZTcheck = ZTCheck()
 
 try:
     LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
@@ -58,12 +52,13 @@ async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
+    LOGS.info(f"⌔ تـم تنصيـب زدثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر ارسـل (.الاوامر)")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    Catcheck.sucess = True
+    ZTcheck.sucess = True
     return
 
 
@@ -71,9 +66,11 @@ zedub.loop.run_until_complete(startup_process())
 
 if len(sys.argv) not in (1, 3, 4):
     zedub.disconnect()
-elif not Catcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
+elif not ZTcheck.sucess:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
 else:
     try:
         zedub.run_until_disconnected()
