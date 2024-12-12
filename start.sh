c90e2c2b09af61897a6e30b7464b1c4675fc2527
Heroku-Back <143542266+Heroku-Back@users.noreply.github.com>
2024-12-12 21:27:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-
 curl -sL https://deb.nodesource.com/setup_20.x | bash - && apt-get install -y nodejs && apt-get -qq update && apt-get -qq install -y git wget ffmpeg mediainfo && pip install --upgrade pip && pip3 install --user -r requirements.txt
