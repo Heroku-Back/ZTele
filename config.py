d11a08e6c6cb236f92020c9b52f1bad475c16277
Heroku-Back <143542266+Heroku-Back@users.noreply.github.com>
2024-12-12 19:50:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+APP_ID = int(environ.get("APP_ID", "27455984"))
+API_HASH = environ.get("API_HASH", "62d5f68ce2e9189636967120220f5755")
+TG_BOT_TOKEN = environ.get("TG_BOT_TOKEN", "")
+STRING_SESSION = environ.get("STRING_SESSION", "")
+DB_URI = environ.get("DB_URI", "")
